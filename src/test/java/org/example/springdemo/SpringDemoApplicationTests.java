ac591a3fc05588ad55dd67372f35da89b60e5333
LEO MENG <mengle@oocl.com>
2025-09-09 17:28:28 +0800

    test:add test should_return_an_employee_when_get_given_a_valid_id

--- src/test/java/org/example/springdemo/SpringDemoApplicationTests.java
+++ src/test/java/org/example/springdemo/SpringDemoApplicationTests.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,7 +28,6 @@ class SpringDemoApplicationTests {
     void should_create_employee_when_post_given_a_valid_body() throws Exception{
         String requestBody = """
                   {
-                       "id": 11,
                        "name": "Tom",
                        "age": 20,
                        "salary": 5000.0,
@@ -38,6 +38,30 @@ class SpringDemoApplicationTests {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").value(1));
 
+    }
+
+    @Test
+    void should_return_employee_when_get_given_a_valid_id() throws Exception{
+        String requestBody = """
+                  {
+                       "name": "Tom",
+                       "age": 20,
+                       "salary": 5000.0,
+                       "gender": "male"
+                   }
+                """;
+        mockMvc.perform(post("/employees").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1));
+
+
+        mockMvc.perform(get("/employees/{id}",1).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Tom"))
+                .andExpect(jsonPath("$.age").value(20))
+                .andExpect(jsonPath("$.salary").value(5000.0))
+                .andExpect(jsonPath("$.gender").value("male"));
     }
 
 }
